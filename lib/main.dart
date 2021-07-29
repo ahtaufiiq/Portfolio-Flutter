19fa13f1ef05d26c01501b47e354dd0425937cd2
Ahmad Taufiq <ataufiq665@gmail.com>
2021-07-29 09:59:45 +0700

    linear-gradient indicator setiap story

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,103 @@
+import 'package:coba/story_view.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.green,
         ),
-        home: HomeScreen());
+        home: Home());
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class Home extends StatelessWidget {
+  final StoryController controller = StoryController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: SafeArea(
-        child: Container(
-          child: Center(child: Text("Home")),
+      appBar: AppBar(
+        title: Text("Delicious Ghanaian Meals"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(
+          8,
         ),
+        child: Stories(),
+      ),
+    );
+  }
+}
+
+class Stories extends StatefulWidget {
+  @override
+  _StoriesState createState() => _StoriesState();
+}
+
+class _StoriesState extends State<Stories> {
+  final storyController = StoryController();
+
+  @override
+  void dispose() {
+    storyController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("More"),
+      ),
+      body: StoryView(
+        storyItems: [
+          StoryItem.text(
+            title: "I guess you'd love to see more of our food. That's great.",
+            backgroundColor: Colors.blue,
+          ),
+          StoryItem.text(
+            title: "Nice!\n\nTap to continue.",
+            backgroundColor: Colors.red,
+            textStyle: TextStyle(
+              fontFamily: 'Dancing',
+              fontSize: 40,
+            ),
+          ),
+          StoryItem.pageImage(
+            url:
+                "https://image.ibb.co/cU4WGx/Omotuo-Groundnut-Soup-braperucci-com-1.jpg",
+            caption: "Still sampling",
+            controller: storyController,
+          ),
+          StoryItem.text(
+            title: "I guess you'd love to see more of our food. That's great.",
+            backgroundColor: Colors.green,
+          ),
+          StoryItem.text(
+            title: "Nice!\n\nTap to continue.",
+            backgroundColor: Colors.red,
+            textStyle: TextStyle(
+              fontFamily: 'Dancing',
+              fontSize: 40,
+            ),
+          ),
+        ],
+        onStoryShow: (s) {
+          print("Showing a story");
+        },
+        onComplete: () {
+          print("Completed a cycle");
+        },
+        progressPosition: ProgressPosition.top,
+        repeat: false,
+        controller: storyController,
       ),
     );
   }
