aa892088636b9998bbf76141e8a32c71a7081b17
Ahmad Taufiq <ataufiq665@gmail.com>
2021-07-31 20:50:41 +0700

    rapihin

--- lib/screens/markdown.dart
+++ lib/screens/markdown.dart
@@ -3,22 +3,90 @@ import 'package:flutter/material.dart';
 
 final Color darkBlue = const Color.fromARGB(255, 18, 32, 47);
 
-class Markdown extends StatelessWidget {
+class Markdown extends StatefulWidget {
+  const Markdown();
+  _MarkdownState createState() => _MarkdownState();
+}
+
+class _MarkdownState extends State<Markdown> {
+  final TextEditingController _controller = TextFieldFormatting({
+    r"@.\w+": TextStyle(color: Colors.blue),
+    'red': const TextStyle(
+        color: Colors.red, decoration: TextDecoration.underline),
+    'green': TextStyle(color: Colors.green),
+    'purple': TextStyle(color: Colors.purple),
+    r'_(.*?)\_': TextStyle(fontStyle: FontStyle.italic),
+    '~(.*?)~': TextStyle(decoration: TextDecoration.lineThrough),
+    r'\*(.*?)\*': TextStyle(fontWeight: FontWeight.bold),
+    r'```(.*?)```': TextStyle(
+      color: Colors.yellow,
+      fontFeatures: [const FontFeature.tabularFigures()],
+    ),
+  });
+
+  void formatText(_controller, pola) {
+    var awal = _controller.text.substring(0, _controller.selection.baseOffset);
+    var akhir = _controller.text
+        .substring(_controller.selection.extentOffset, _controller.text.length);
+    if (awal[awal.length - 1] == pola && akhir[akhir.length - 1] == pola) {
+      awal = awal.substring(0, awal.length - 1);
+      akhir = akhir.substring(1, akhir.length);
+    } else {
+      awal += pola;
+      akhir = pola + akhir;
+    }
+    var tengah = '${_controller.selection.textInside(_controller.text)}';
+    _controller.selection = TextSelection.fromPosition(
+        TextPosition(offset: _controller.selection.extentOffset));
+    _controller.text = awal + tengah + akhir;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: MyWidget(),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              style: const TextStyle(fontSize: 20),
+              controller: _controller,
+            ),
+            Row(
+              children: [
+                TextButton(
+                    onPressed: () {
+                      formatText(_controller, "*");
+                    },
+                    child: Text("Bold")),
+                TextButton(
+                    onPressed: () {
+                      formatText(_controller, "_");
+                    },
+                    child: Text("Italic")),
+                TextButton(
+                    onPressed: () {
+                      formatText(_controller, "~");
+                    },
+                    child: Text("Coret")),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-class TextFieldColorizer extends TextEditingController {
+class TextFieldFormatting extends TextEditingController {
   final Map<String, TextStyle> map;
   final Pattern pattern;
 
-  TextFieldColorizer(this.map)
+  TextFieldFormatting(this.map)
       : pattern = RegExp(
             map.keys.map((key) {
               return key;
@@ -85,83 +153,4 @@ class TextFieldColorizer extends TextEditingController {
 
     return TextSpan(style: style, children: children);
   }
-}
-
-class MyWidget extends StatefulWidget {
-  const MyWidget();
-  _MyWidgetState createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
-  final TextEditingController _controller = TextFieldColorizer(
-    {
-      r"@.\w+": TextStyle(color: Colors.blue),
-      'red': const TextStyle(
-          color: Colors.red, decoration: TextDecoration.underline),
-      'green': TextStyle(color: Colors.green),
-      'purple': TextStyle(color: Colors.purple),
-      r'_(.*?)\_': TextStyle(fontStyle: FontStyle.italic),
-      '~(.*?)~': TextStyle(decoration: TextDecoration.lineThrough),
-      r'\*(.*?)\*': TextStyle(fontWeight: FontWeight.bold),
-      r'```(.*?)```': TextStyle(
-        color: Colors.yellow,
-        fontFeatures: [const FontFeature.tabularFigures()],
-      ),
-    },
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            keyboardType: TextInputType.multiline,
-            maxLines: null,
-            style: const TextStyle(fontSize: 20),
-            controller: _controller,
-          ),
-          Row(
-            children: [
-              TextButton(
-                  onPressed: () {
-                    formatText(_controller, "*");
-                  },
-                  child: Text("Bold")),
-              TextButton(
-                  onPressed: () {
-                    formatText(_controller, "_");
-                  },
-                  child: Text("Italic")),
-              TextButton(
-                  onPressed: () {
-                    formatText(_controller, "~");
-                  },
-                  child: Text("Coret")),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-void formatText(_controller, pola) {
-  var awal = _controller.text.substring(0, _controller.selection.baseOffset);
-  var akhir = _controller.text
-      .substring(_controller.selection.extentOffset, _controller.text.length);
-  if (awal[awal.length - 1] == pola && akhir[akhir.length - 1] == pola) {
-    awal = awal.substring(0, awal.length - 1);
-    akhir = akhir.substring(1, akhir.length);
-  } else {
-    awal += pola;
-    akhir = pola + akhir;
-  }
-  var tengah = '${_controller.selection.textInside(_controller.text)}';
-  _controller.selection = TextSelection.fromPosition(
-      TextPosition(offset: _controller.selection.extentOffset));
-  _controller.text = awal + tengah + akhir;
 }
