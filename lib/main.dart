f9f63924c6249108da1d70a6ef4d72f8d61a9716
Ahmad Taufiq <ataufiq665@gmail.com>
2021-08-03 14:04:40 +0700

    load more

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,100 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: HomeScreen());
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int present = 0;
+  int perPage = 15;
+  int counter = 0;
+
+  final originalItems = List<String>.generate(10000, (i) => "Item $i");
+  var items = ["kata"];
+
+  late Timer searchOnStoppedTyping;
+
+  _onChangeHandler(value) {
+    const duration = Duration(
+        milliseconds:
+            1500); // set the duration that you want call search() after that.
+    setState(() => searchOnStoppedTyping =
+        new Timer(duration, () => print("Tes"))); // clear timer
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    searchOnStoppedTyping =
+        Timer(Duration(seconds: 3), () => {print("testing lagi")});
+    setState(() {
+      items.addAll(originalItems.getRange(present, present + perPage));
+      present = present + perPage;
+    });
+  }
+
+  void loadMore() {
+    setState(() {
+      counter++;
+      print(counter);
+      searchOnStoppedTyping =
+          Timer(Duration(seconds: 3), () => {print("testing lagi")});
+      items.addAll(originalItems.getRange(present, present + perPage));
+      present = present + perPage;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
+    return new Scaffold(
       body: SafeArea(
-        child: Container(
-          child: Center(child: Text("Home")),
+        child: NotificationListener<ScrollNotification>(
+          onNotification: (ScrollNotification scrollInfo) {
+            if (scrollInfo.metrics.pixels >
+                scrollInfo.metrics.maxScrollExtent - 20 && !searchOnStoppedTyping.isActive) {
+              loadMore();
+            }
+            return false;
+          },
+          child: ListView.builder(
+            itemCount: (present <= originalItems.length)
+                ? items.length + 1
+                : items.length,
+            itemBuilder: (context, index) {
+              return (index == items.length)
+                  ? Container(
+                      color: Colors.greenAccent,
+                      child: TextButton(
+                        child: Text("Load More"),
+                        onPressed: () {
+                          loadMore();
+                        },
+                      ),
+                    )
+                  : ListTile(
+                      title: Text('${items[index]}'),
+                    );
+            },
+          ),
         ),
       ),
     );
